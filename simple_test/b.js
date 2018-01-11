a4b7acb42275665d18fb1f79438090a2b21a50db
Globik <gru5@yandex.ru>
2018-01-11 12:00:20 -0500

    I got it!

--- simple_test/b.js
+++ simple_test/b.js
@@ -1,44 +1,23 @@
 const addon=require('./build/Release/test_async');
-
-//STEP 1 is OK
-var fl=0;
-addon.Test(5,{type:'stakkkrt'},function(err,val){
+addon.Test(15,{type:'start'},function(err,val){
 	console.log('err: ',err);
 	console.log('val: ',val);
-	
-	//addon.Test(5,{type:'start'},function(err, val){
-//console.log(err);
-//console.log(val);
-//})
 });
 
-// STEP 2  and further CRASHES
-// RACE CONDITION OR WHAT? 
-//Console error report:
-//undefined:0
-//Error: Invalid argument
+addon.Test(2,{type:'start'},function(err, val){
+console.log('err: ',err);
+console.log('val: ',val);
+})
 
-/*
-addon.Test(5,{type:'start'},function(err, val){
-console.log(err);
-console.log(val);
-})
-*/var i=0;
-/*
-setTimeout(function(){
+var i=0;
+var t=setInterval(function(){
 	i++;
-	//if(fl==1)
-addon.Test(5+i,{type:'start'},function(err, val){
-console.log(err);
-console.log(val);
+addon.Test(4,{type:'start'},function(err, val){
+console.log('err: ',err);
+console.log('val: ',val);
+console.log('I: ',i);
+if(i==300){clear();}
 })
-},19);*/
-setInterval(function(){
-setImmediate(()=>{
-	i++;
-addon.Test(1+i,{type:'start'},function(err, val){
-console.log(err);
-console.log(val);
-})
-})
-},4)
+},0)
+function clear(){clearInterval(t);}
+// I got it! Thank you to all.
--- simple_test/test_async.cc
+++ simple_test/test_async.cc
@@ -1,21 +1,3 @@
-/*
-var addon=require("./build/Release/test_async");
-// colling a method Test - first time it's OK
-
-addon.Test(5,{type:'start'},function(err, val){
-console.log(err);
-console.log(val);// 10
-})
-
-// but right after it it's not OK, it's crashed, but why??? Race condition???
-
-addon.Test(5,{type:'start'},function(err, val){
-console.log(err);
-console.log(val);// 10
-})
-
-*/
-
 #include <node_api.h>
 #include "common.h"
 #include <stdlib.h>
@@ -27,19 +9,19 @@ console.log(val);// 10
 #include <unistd.h>
 #endif
 
-typedef struct{
+typedef struct carrier{
 int32_t _input;
 int32_t _output;
 napi_ref _callback;
 napi_async_work _request;
 }carrier;
-carrier the_carrier;
+
 
 void Execute(napi_env env,void*data){
 carrier* c=(carrier*)data;
-	if(c !=&the_carrier){napi_throw_type_error(env,nullptr,"wrong data to execute.\n");return;}
+	//if(c !=&the_carrier){napi_throw_type_error(env,nullptr,"wrong data to execute.\n");return;}
 	#if defined _WIN32
-	Sleep(1000);
+	//Sleep(1000);
 	#else
 	//sleep(1);
 	#endif
@@ -49,17 +31,17 @@ carrier* c=(carrier*)data;
 //fig
 void Complete(napi_env env,napi_status status, void* data){
     carrier* c=(carrier*)data;
-	
+	/*
 	if(c !=&the_carrier){
 	napi_throw_type_error(env,nullptr,"wrong data parameter to Complete");
 		return;
-	}
+	}*/
 	
 	if(status !=napi_ok){
 	napi_throw_type_error(env,nullptr,"execute callback failed.");
 		return;
 	}
-    napi_handle_scope scope;
+    //napi_handle_scope scope;
 	napi_value argv[2];
 	//napi_open_handle_scope(env,&scope);
 	printf("satus: %s\n",(char*)status);
@@ -80,41 +62,32 @@ void Complete(napi_env env,napi_status status, void* data){
 	NAPI_CALL_RETURN_VOID(env, napi_delete_async_work(env,c->_request));
 	//napi_close_handle_scope(env,scope);
 	//return;
+	free(c);// ???
 }
 
 napi_value Test(napi_env env,napi_callback_info info){
-	napi_handle_scope scope;
+	//napi_handle_scope scope;
+	carrier* c=(carrier*)malloc(sizeof(carrier));
 size_t argc=3;
 	
 	napi_value argv[3];
-	napi_value _this;
+	napi_value _this; // <=FOR WHAT THIS _THIS???
 	napi_value resource_name;
-	void* data;
+	void* data;// <= FOR WHAT THIS DATA??? c->data=data???
 	//napi_open_handle_scope(env,&scope);
 	
 	NAPI_CALL(env,napi_get_cb_info(env,info,&argc,argv,&_this,&data));
-	
 	//NAPI_ASSERT(env,argc >=3,"not enough arguments, expected 3?.");
+	c->_output=0;
+	NAPI_CALL(env,napi_get_value_int32(env,argv[0],&c->_input));
 	
+	NAPI_CALL(env, napi_create_reference(env,argv[2],1,&c->_callback));
 	
-	the_carrier._output=0;
-	NAPI_CALL(env,napi_get_value_int32(env,argv[0],&the_carrier._input));
+	NAPI_CALL(env, napi_create_string_utf8(env,"TestResource",NAPI_AUTO_LENGTH, &resource_name));
 	
+	NAPI_CALL(env, napi_create_async_work(env,argv[1],resource_name,Execute,Complete,c, &c->_request));
 	
-	NAPI_CALL(env, napi_create_reference(env,argv[2],1,&the_carrier._callback));
-	
-	NAPI_CALL(env, napi_create_string_utf8(env,"TestResource",NAPI_AUTO_LENGTH,&resource_name));
-	
-	NAPI_CALL(env, napi_create_async_work(env,argv[1],resource_name,Execute,Complete,&the_carrier,&the_carrier._request));
-	
-	NAPI_CALL(env, napi_queue_async_work(env,the_carrier._request));
-	
-	//NAPI_CALL(env, napi_create_async_work(env,argv[1],resource_name,Execute,Complete,&the_carrier,&the_carrier._request));
-	
-	//NAPI_CALL(env, napi_queue_async_work(env,the_carrier._request));
-	
-	
-	//napi_close_handle_scope(env,scope);
+	NAPI_CALL(env, napi_queue_async_work(env,c->_request));
 	return nullptr;
 	}
 
@@ -127,5 +100,5 @@ napi_property_descriptor properties[]={
 NAPI_CALL(env, napi_define_properties(env,exports,sizeof(properties)/sizeof(*properties),properties));
 return exports;
 }
-//NAPI_MODULE(NODE_GYP_MODULE_NAME, init)
+
 NAPI_MODULE(addon,Init);
